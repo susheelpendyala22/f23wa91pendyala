67b8f0345cd137c1ac826f42eda414319577f52d
Susheel_Pendyala <pendyalasusheel@gmail.com>
2023-10-24 18:41:14 -0500

    added computation.js with bonus

--- routes/computation.js
+++ routes/computation.js
@@ -5,13 +5,13 @@ router.get("/", function (req, res, next) {
  var rndNumArr = Math.round(randNumArr);
  if (req.query.x) {
   let x = req.query.x;
-  var arr = [Math.atan(x), Math.exp(x), Math.expm1(x)];
+  var arr = [Math.imul(), Math.exp(x), Math.expm1(x)];
   var funObj = {
    0: "Math.imul()",
    1: "Math.log()",
    2: "Math.log10()",
   };
-  var arr = [Math.atan(x), Math.exp(x), Math.expm1(x)];
+  var arr = [Math.imul(), Math.exp(x), Math.expm1(x)];
   let response1 = `
   Function ${funObj[rndNumArr]} applied to x: ${x} the result is ${arr[rndNumArr].toFixed(3)}`;
   res.send(response1);
@@ -26,7 +26,7 @@ router.get("/", function (req, res, next) {
     1: "Math.log()",
     2: "Math.log10()",
   };
-  var arr = [Math.atan(x), Math.exp(x), Math.expm1(x)];
+  var arr = [Math.imul(), Math.exp(x), Math.expm1(x)];
   let response = `
   Function ${funObj[rndNumArr]} applied to the random value for x from 1 to 10 is: ${x} and the result is ${arr[rndNumArr].toFixed(3)}`
   res.send(response);
