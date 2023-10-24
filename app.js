fcbd2f3c7dae5f8bd46b88bd64e879c5c87ef90b
Susheel_Pendyala <pendyalasusheel@gmail.com>
2023-10-24 16:48:02 -0500

    modified app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var mydata = require('./routes/mydata');
+var computation = require('./routes/computation');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', mydata);
+app.use('/', computationRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
