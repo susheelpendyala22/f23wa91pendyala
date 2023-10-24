7410f0dcdf39a05cc3ac1cd8f4fdaa13733be8cf
Susheel_Pendyala <pendyalasusheel@gmail.com>
2023-10-24 13:38:35 -0500

    modified app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var mydata = require('./routes/index');
+var mydata = require('./routes/mydata');
 var usersRouter = require('./routes/users');
 
 var app = express();
