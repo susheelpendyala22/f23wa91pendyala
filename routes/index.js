3f1235926b0fc7bcb47882c82f65c38d9202a668
Susheel_Pendyala <pendyalasusheel@gmail.com>
2023-10-24 13:51:08 -0500

    modified index.js

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'express' });
 });
 
 module.exports = router;
-
-app.listen(3001, function () {
-  console.log('Example app listening on port ' + port + '!');
-});
